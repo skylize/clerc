c1d1c8104f5d4aeb7a385fb78f00c3795a2bdd79
Sky Higgins <eveningsky@gmail.com>
2017-09-12 18:07:47 -0400

    add webpack, rm gulp and gulp-related deps
    * Adds webpack and several webpack plugins and a fairly basic build script.
    * Removes gulp and gulp build scripts.
    * Removes large list of depencencies required by gulp build process.

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+
+const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin')
+const CleanPlugin = require('clean-webpack-plugin')
+const LiveReload = require('webpack-watch-livereload-plugin')
+
+console.log('env', process.env.NODE_ENV)
+
+const plugins = ( () => {
+  const plugins = []
+  process.env.NODE_ENV === 'production'
+    && plugins.push(new CleanPlugin('build')),
+  process.env.NODE_ENV !== 'production'
+    && plugins.push(new LiveReload('build/*'))
+  return plugins
+})()
+
+const version = require('./package.json').version
+
+module.exports = {
+  context: path.join(__dirname, 'src'),
+  entry: {
+    'background': './background.js',
+    'manifest.json': './manifest.json',
+    'img': './img/index.js',
+    'fakie': '../fake-plugin/background.js'
+  },
+  plugins,
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      { // add version from package.json to manifest
+        test: /manifest\.json$/,
+        use: [
+          'file-loader?name=manifest.json',
+          'json-loader',
+          {
+            loader: 'str-replace-loader',
+            options: {
+              match: '{{version}}',
+              replace: version,
+            }
+          }
+        ],
+      },
+      { //copy image files
+        test: /\.png|svg|jpg|gif$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+              useRelativePath: true
+            }
+          },{
+            loader: 'image-webpack-loader',
+            options: {
+              optipng: {
+                optimizationLevel: 7,
+              },
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
