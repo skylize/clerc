7b82fe9f6d0fd38a6c363bad9507e269cb2e2be2
Sky Higgins <eveningsky@gmail.com>
2017-09-12 18:07:47 -0400

    add webpack, rm gulp and gulp-related deps
    * Adds webpack and several webpack plugins and a fairly basic build script.
    * Removes gulp and gulp build scripts.
    * Removes large list of depencencies required by gulp build process.

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+
+const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin')
+const CleanPlugin = require('clean-webpack-plugin')
+const LiveReload = require('webpack-watch-livereload-plugin')
+
+const plugins = ( () => {
+  const all = []
+
+  const production = [
+    new CleanPlugin('build')
+  ]
+
+  const dev = [
+    new LiveReload('build/*')
+  ]
+
+  return all.concat(
+    process.env.NODE_ENV === 'production'
+      ? production
+      : dev )
+})()
+
+const version = require('./package.json').version
+
+module.exports = {
+  context: path.join(__dirname, 'src'),
+  entry: {
+    'background': './background.js',
+    'manifest': './manifest.json',
+    'img': './img/index.js',
+    'fakie': '../fake-plugin/background.js'
+  },
+  plugins,
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      { // add version from package.json to manifest
+        test: /manifest\.json$/,
+        use: [
+          'file-loader?name=manifest.json',
+          {
+            loader: 'str-replace-loader',
+            options: {
+              match: '{version}',
+              replace: version,
+            }
+          }
+        ],
+      },
+      { //copy image files
+        test: /\.png|svg|jpg|gif$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+              useRelativePath: true
+            }
+          },{
+            loader: 'image-webpack-loader',
+            options: {
+              optipng: {
+                optimizationLevel: 7,
+              },
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
