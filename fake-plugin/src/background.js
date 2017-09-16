ca620f39a81cf3b4e9f73b7e3a7b337f9335289a
Sky Higgins <eveningsky@gmail.com>
2017-09-16 16:09:10 -0400

    make console logs from fake-plugin slightly more clear

--- fake-plugin/src/background.js
+++ fake-plugin/src/background.js
@@ -6,12 +6,11 @@ console.log('Fake plugin started')
 chrome.runtime.onMessageExternal.addListener(
   (msg, sender, resp) => {
 
-    console.log({msg, sender, resp})
+    console.log('recieved message',{msg, sender, resp})
     // reload if you get a reload command
     if (msg.command && msg.command === 'reload')
     {
       console.log('received reload command')
     }
-    
   }
 )
