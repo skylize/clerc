4e01318a51c388ceb62ccb543007d9a61c6be48e
Sky Higgins <eveningsky@gmail.com>
2017-09-17 19:43:13 -0400

    double-check readyState on open, timeout long tests
    * A socket may not actually be ready when it emits the open event. Added a function to poll readyState to find out when it's really open.
    
    * Reworked test to accept with this change and added tests to be sure it accepts delayed ready state, but doesn't wait forever.
    
    * Added timeouts to all async tests so they cannot run forever if broken. If async only used as convenience for unwrapping promises, just timeout to next-tick.
    
    * Also moved stuff around and did very minor refactoring to make code a little cleaner, easier to follow.

--- src/livereload.js
+++ src/livereload.js
@@ -1,4 +1,6 @@
 
+import promise from 'empty-promise'
+
 const handshake = JSON.stringify({
   command: 'hello',
   protocols: [
@@ -11,33 +13,60 @@ const handshake = JSON.stringify({
   ],
 })
 
-// Let's give consumer some nice feedback if she forgets a dependency.
+// Instead of throwing. Let's provide some defaults that
+// give consumer dev some feedback about missing dependency
+// without completely failing.
 const unimplemented = {
-  onOpen: ()=> console.log('no onOpen listener'),
+  onOpen: ()=> console.log('Missing onOpen listener.'),
 
-  onClose: ()=> console.log('no onClose listener'),
+  onClose: ()=> console.log('Missing onClose listener.'),
 
-  onError: e=>
-    console.error('error thrown, but no callback'
-      + ' to pass it to.', e),
-
-  // This dependency is special. Throw instead of warn.
-  WebSocket: function (){
-    throw 'missing WebSocket Constructor'
+  onError: e=> {
+    console.log('Error thrown, but no onError to pass it to.')
+    console.error(e)
   },
 
   messenger: {
     message: msg=> console.log(
-      'No messenger to hear this message: ',
+      'Missing messenger. \nmessage:',
       msg),
   },
+
+  // This dependency is special. Can't even try to do
+  // anything without it. So throw instead of warn.
+  WebSocket: function (){
+    throw 'Missing WebSocket constructor.'
+  },
+
 }
-
 /**
+ * Factory provider of a LiveReload client implentation
  *
+ * @param {Number}    port        port for server
+ *
+ * @param {String}    host        hostname for server
+ *
+ * @param {Function}  onOpen      Callback after connection is
+ *                                opened.
+ *
+ * @param {Function}  onClose     Callback after connection is
+ *                                closed
+ *
+ * @param {Function}  onError     Callback after any error.
+ *                                This module tries to suppress
+ *                                thrown errors, and instead
+ *                                passes them to onError.
+ *
+ * @param {Function} WebSocket   An implentation of WebSocket
+ *
+ * @param {Object}    messenger   An object with a `message`
+ *                                method, which accepts an
+ *                                object parameter.
+ *
+ * @return {Object}               API object with 2 methods:
+ *                                `connect` and `disconnect`
  */
-
-const makeLivereloadSocket =  ({
+function makeLivereloadSocket ({
   // Seems reasonable to use LiveReload's default
   // port on localhost if none provided.
   port = 35729,
@@ -45,21 +74,15 @@ const makeLivereloadSocket =  ({
   onOpen = unimplemented.onOpen,
   onClose = unimplemented.onClose,
   onError = unimplemented.onError,
-  WebSocket = unimplemented.WebSocket,
   messenger = unimplemented.messenger,
-})=> {
+  WebSocket = unimplemented.WebSocket,
+}) {
 
   let socket // insert active socket here ;)
 
-  function parseMsg(evt) {
-    let msg
-    try { msg = JSON.parse(evt.data) }
-    catch (e) { onError(
-      new Error('Invalid JSON from LiveReload server.')
-    )}
-    return msg
-  }
-
+  // Initiate connection and add listeners
+  // Publicly exposed API method.
+  //
   function connect() {
     // nothing to do if already connected
     if ( socket && socket.readyState === 1) return
@@ -70,34 +93,84 @@ const makeLivereloadSocket =  ({
     }
     catch(e) { return void onError(e) }
 
-    socket.onopen = onSocketOpen
+    socket.onmessage = hello
     socket.onerror = onError
     socket.onclose = onSocketClose
+    socket.onopen = async ()=> {
+      const fullyOpened = await yesItsReallyOpen()
+      if(fullyOpened) socket.send(handshake)
+      else socket.close()
+    }
 
     return socket
+  }
+
+  // Close the socket
+  // Publicly exposed API method
+  //
+  function disconnect() {
+    if (!socket || !socket.close)
+      return void onError(
+        new Error('Invalid Disconnect: Cannot close a '
+          + 'socket that has never been opened.')
+      )
+    socket.close()
+    return socket
+  }
+
+  // Turns out a websocket can emit `open` before it's
+  // really open 😡. Let's wait until it's open-state is
+  // fully resolved.
+  //
+  function yesItsReallyOpen(){
+    const isOpen = promise()
+
+    const poll = ()=> {
+      if(socket.readyState === 1)
+        isOpen.resolve(true)
+
+      else setTimeout(poll, 100)
+    }
+
+    // Race against a timeout. 8 seconds is long time to
+    // wait for this. But hey, why not?
+    setTimeout(() => {isOpen.reject(
+      new Error('Timeout: WebSocket half-open too long.'))
+    }, 8000),
+
+    poll()
+    return isOpen
+      .catch(e => { onError(e); return false })
+  }
+
+  // Try to parse JSON and pass any error to consumer
+  //
+  function parseMsg(evt) {
+    let msg
+    try { msg = JSON.parse(evt.data) }
+    catch (e) { onError(
+      new Error('Invalid JSON from LiveReload server.')
+    )}
+    return msg
   }
 
   // Must receive hello from server before other messages.
   //
   function hello(evt) {
     const msg = parseMsg(evt)
-    if (!msg)
+    const helloError =
+      new Error('Invalid "hello" from server')
+
+    if (!msg || msg.command !== 'hello') {
+      onError(helloError)
       return void socket.close()
-
-    if (msg.command === 'hello')
-      listen()
-    else {
-      socket.close()
-      onError(
-        new Error('Invalid "hello" from server'))
     }
+
+    listen()
   }
 
-  function onSocketOpen() {
-    socket.send(handshake)
-    socket.onmessage = hello
-  }
-
+  // Switch to listening for arbitrary incoming messages.
+  //
   function listen() {
     socket.onmessage = evt=> {
       const msg = parseMsg(evt)
@@ -107,21 +180,21 @@ const makeLivereloadSocket =  ({
       if (msg.command === 'reload')
         messenger.message(msg)
     }
-    onOpen()
+    // Finally reached state consumer would view as open
+    onOpen() // consumer provided callback
   }
 
-  function disconnect() {
-    if (socket && socket.close) socket.close()
-    return socket
-  }
-
+  // Let consumer know we're closed for business.
+  // Some light cleanup might help garbage collection.
+  //
   function onSocketClose() {
-    // a little light cleanup
+    // some light cleanup
     ['onclose', 'onopen', 'onerror', 'onmessage']
       .forEach(listener=>
         delete socket[listener])
+    socket = void 0
 
-    onClose()
+    onClose() // consumer provided callback
   }
 
   return { connect, disconnect }
--- src/livereload.test.js
+++ src/livereload.test.js
@@ -1,176 +1,320 @@
 import test from 'ava'
-import sinon from 'sinon'
-import EmptyPromise from 'empty-promise'
+import promise from 'empty-promise'
 import makeLrSocket from './livereload.js'
 
-// just for reference when writing tests
-const sampleServerHello = { /* eslint-disable quotes */
-  "command": "hello",
-  "protocols": [
-    "http://livereload.com/protocols/official-7",
-  ],
-  "serverName":"tiny-lr",
-} /* eslint-enable quotes */
 
+/**
+ * Some convenience values
+ */
+
+// TODO test can handle other ports
 const ports =  {
+  // This is the default LiveReload port, so most likely
+  // to be used.
   valid: 35729,
+  // We need a different port to also try as valid because
+  // module uses 35729 as default internally.
+  different_valid: 32290,
+  // Nothing wrong with 55555,just a number that
+  // is different from ones we consider valid in tests.
   invalid: 55555,
 }
+// TODO test can handle other hosts
 const hosts = {
-  valid: 'localost',
+  valid: 'localhost',
+  different_valid: '129.0.0.1',
   invalid: 'wronghost',
 }
-const helloEvent = JSON.stringify({
-  data: {command: 'hello'},
-})
-const reloadEvent = JSON.stringify({
-  data: {command: 'reload'},
-})
+
+// Events to pass into WebSocket.onmessage()
+//
+const helloEvent = {
+  data: JSON.stringify(
+    {command: 'hello'}
+  ),
+}
+const reloadEvent = {
+  data: JSON.stringify(
+    {command: 'reload'}
+  ),
+}
+
+/**
+ *
+*  - Each test implements the minimum required mocking of
+*    WebSocket in order to verify behavior.
+*
+*  - Using empty promises as spies. These are promises with
+*    no internal content. Instead they have a small external
+*    API, of `resolve(val)`, `reject(val)`, and `done()`.
+*    The `done` method returns `true` if unresolved, or
+*    `false` if resolved/rejected.
+*
+*  - Rather than emit events, just call the attached listener
+*    by name.
+*
+*  - Tests should not run forever. To ensure broken tests do
+*    not hang async tests must include a timeout to fail() or
+*    reject an awaited promise. Timers should start at start
+*    of test before any async keywords.
+*
+*  - Set timeout of 0 for next-tick if awaited promise is
+*    a spy that should resolve within the same event loop.
+*
+*/
 
 test('connects to websocket', t=> {
-  const spy = sinon.spy()
+  const spy = promise()
   const lrSocket = makeLrSocket({
     port: ports.valid,
     host: hosts.valid,
     messenger: {message(){}},
-    WebSocket: spy,
+
+    WebSocket: function WebSocket(){
+      spy.resolve()
+    },
   })
+
   lrSocket.connect()
-  t.is(spy.called, true)
+
+  t.is(spy.done(), true)
 })
 
-test('sends a hello', t=> {
-  const spy = sinon.spy()
+/**
+ *
+ */
 
-  function WebSocket() {
-    this.send = spy
-  }
+test('sends a hello', async t=> {
+  setTimeout(()=> t.fail('Test took too long.'), 0)
+
+  const spy = promise()
 
   const lrSocket = makeLrSocket({
     port: ports.valid,
     host: hosts.valid,
     messenger: {message(){}},
-    WebSocket,
+
+    WebSocket: function WebSocket() {
+      this.send = spy.resolve
+      this.readyState = 1
+    },
   })
 
-  const websocket = lrSocket.connect()
-  websocket.onopen()
+  const socket = lrSocket.connect()
+  socket.onopen()
 
-  t.is(spy.called, true)
-  t.is(JSON.parse(spy.args[0][0]).command, 'hello')
+  t.is(JSON.parse(await spy).command, 'hello')
 })
 
-test('hangs up if no hello', t=> {
-  const spy = sinon.spy()
+/**
+ *
+ */
 
-  function WebSocket() {
-    this.send = ()=> {},
-    this.close = spy
-  }
+test('hangs up if hello is not valid', t=> {
+  const spy = promise()
 
   const lrSocket = makeLrSocket({
     port: ports.valid,
     host: hosts.valid,
     messenger: {message(){}},
-    WebSocket,
     onError: ()=> {},
+
+    WebSocket: function WebSocket() {
+      this.send = ()=> {},
+      this.close = spy.resolve
+    },
   })
 
-  const websocket = lrSocket.connect()
-  websocket.onopen()
-  websocket.onmessage('bad string')
+  const socket = lrSocket.connect()
+  socket.onopen()
+  socket.onmessage('bad string')
 
-  t.is(spy.called, true)
+  t.is(spy.done(), true)
 })
+
+/**
+ *
+ */
 
 test('fails to create socket with invalid host', t=> {
   const validUrl =
     `ws://${hosts.valid}:${ports.valid}/livereload`
 
-  function WebSocket(url) {
-    if (url !== validUrl) throw 'bad url'
-  }
-
-  const spy = sinon.spy()
+  const spy = promise()
   const lrSocket = makeLrSocket({
     port: ports.valid,
     host: hosts.invalid,
     messenger: {message(){}},
-    WebSocket,
-    onError: spy,
+    onError: spy.resolve,
+
+    WebSocket: function WebSocket(url) {
+      if (url !== validUrl) throw 'bad url'
+    },
   })
 
-  const websocket = lrSocket.connect()
+  const socket = lrSocket.connect()
 
-  t.is(websocket, void 0)
-  t.is(spy.called, true)
+  t.is(socket, void 0)
+  t.is(spy.done(), true)
 })
 
 test('fails to create socket with invalid port', t=> {
   const validUrl =
     `ws://${hosts.valid}:${ports.valid}/livereload`
 
-  function WebSocket(url) {
-    if (url !== validUrl) throw 'bad url'
-  }
-
-  const spy = sinon.spy()
+  const spy = promise()
   const lrSocket = makeLrSocket({
     port: ports.invalid,
     host: hosts.valid,
     messenger: {message(){}},
-    WebSocket,
-    onError: spy,
+    onError: spy.resolve,
+
+    WebSocket: function WebSocket(url) {
+      if (url !== validUrl) throw 'bad url'
+    },
   })
 
-  const websocket = lrSocket.connect()
+  const socket = lrSocket.connect()
 
-  t.is(websocket, void 0)
-  t.is(spy.called, true)
+  t.is(socket, void 0)
+  t.is(spy.done(), true)
 })
+
+/**
+ *
+ */
 
 test('passes reload message to messenger', async t=> {
-  const receivedHello = EmptyPromise()
+  setTimeout(()=> t.fail('Test took too long.'), 0)
 
-  function WebSocket() {
-    this.send = msg=> {
-      receivedHello.resolve()
-    }
-    this.close = ()=> {}
-  }
+  const helloSpy = promise()
+  const msgSpy = promise()
 
-  const spy = sinon.spy()
   const lrSocket = makeLrSocket({
     port: ports.valid,
     host: hosts.valid,
-    messenger: {message: spy},
-    WebSocket,
+    messenger: {message: msgSpy.resolve},
     onError: ()=> {},
+    onOpen: ()=> {},
+
+    WebSocket: function WebSocket() {
+      this.readyState = 1
+      this.send = helloSpy.resolve
+      this.close = ()=> {}
+    },
   })
-  const websocket = lrSocket.connect()
-  websocket.onopen()
-  websocket.onmessage(helloEvent)
-  await receivedHello
 
-  websocket.onmessage(reloadEvent)
+  const socket = lrSocket.connect()
+  socket.onopen()
 
-  t.is(spy.isCalled)
+  // must send a Hello before a Reload will be accepted
+  socket.onmessage(helloEvent)
+  await helloSpy
+
+  socket.onmessage(reloadEvent)
+
+  t.is(msgSpy.done(), true)
 })
+
+/**
+ *
+ */
 
 test('calls close callback on close', async t=> {
-  const receivedHello = EmptyPromise()
+  setTimeout(()=> t.fail('Test took too long.'), 0)
+  const spy = promise()
 
-  function WebSocket() {}
-
-  const spy = sinon.spy()
   const lrSocket = makeLrSocket({
     port: ports.valid,
     host: hosts.valid,
-    onClose: spy,
-    WebSocket,
-  })
-  const websocket = lrSocket.connect()
-  websocket.onclose()
+    onClose: spy.resolve,
 
-  t.is(spy.called, true)
+    WebSocket: function WebSocket() {},
+  })
+
+  const socket = lrSocket.connect()
+  socket.onclose()
+
+  t.is(spy.done(), true)
 })
+
+/**
+ *
+ */
+
+test('Waits for open readyState after open event', async t=> {
+  setTimeout(()=> t.fail('Test took too long.'), 2000)
+
+  const spy = promise()
+
+  const lrSocket = makeLrSocket({
+    port: ports.valid,
+    host: hosts.valid,
+    messenger: {message(){}},
+
+    WebSocket: function WebSocket() {
+      this.send = spy.resolve
+      this.readyState = 0
+      setTimeout(() => {this.readyState = 1}, 400)
+    },
+  })
+
+  const socket = lrSocket.connect()
+  socket.onopen()
+
+  t.is(
+    JSON.parse(await spy).command,
+    'hello'
+  )
+})
+
+/**
+ *
+ */
+
+test('hangs up if too long to fully connect', async t=> {
+  const testsComplete = promise()
+  setTimeout(()=>
+    testsComplete.reject('Test took too long'),
+  12000)
+
+  const sendSpy = promise()
+  const closeSpy = promise()
+  const errorSpy = promise()
+
+  const lrSocket = makeLrSocket({
+    port: ports.valid,
+    host: hosts.valid,
+    messenger: {message(){}},
+    onError: errorSpy.resolve,
+
+    WebSocket: function WebSocket() {
+      this.send = sendSpy.resolve
+      this.close = closeSpy.resolve
+      this.readyState = 0
+      setTimeout(() => {this.readyState = 1}, 8500)
+    },
+  })
+
+  const socket = lrSocket.connect()
+  socket.onopen()
+
+  setTimeout(async () => {
+    t.is(sendSpy.done(), false)
+    sendSpy.resolve()
+
+    t.is(closeSpy.done(), true)
+    t.is(
+      (await errorSpy).message.indexOf('Timeout') > -1,
+      true
+    )
+
+    testsComplete.resolve()
+  }, 9000)
+
+  await testsComplete
+})
+
+/**
+*
+*/
