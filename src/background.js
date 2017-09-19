01f4aad2a104e4729cf0d41080064945ae0269d4
Sky Higgins <eveningsky@gmail.com>
2017-09-19 17:25:36 -0400

    replace delete listener with undefined assignment
    * Trying to delete throws if listeners have never been assigned. Instead assign void 0 to make them undefined with no gotchas.

--- src/background.js
+++ src/background.js
@@ -1,115 +1,53 @@
 /* global chrome */
 import liveReloadSocket from './livereload.js'
+import makeBadge from './badge.js'
+import makeReload from './reload'
 
-var badge = {
-  badge: 'hide',
-  badges: {
-    hide: [''],
-    ok: [' OK ', '#44992c'],
-    no: [' NO ', '#f33'],
-    connected: ['-(c- ', [42,181,130,200]],
-    // not using disconnect badge b/c visually dominating
-    disconnected: ['-(  c-', [179,179,29,200]],
-  },
-
-  set: function (b){
-    badge.badge = b
-    chrome.browserAction
-      .setBadgeText({text: this.badges[b][0]})
-    if(this.badges[b][1])
-      chrome.browserAction
-        .setBadgeBackgroundColor({color:this.badges[b][1]})
-  },
-
-  tempSet: function (b, delay){
-    var currB = badge.badge
-    if (currB !== b){
-      badge.set(b)
-      badge.delay(currB, delay)
-    }
-  },
-
-  delay: function (b, delay){
-    setTimeout(function (){
-      badge.set(b)
-    }, delay||1200)
-  },
-}
-// we'll start with disconnected badge
-// badge.set('hide')
-
-/////////////////////////////////////////////////////
-
-function reloadExtensions(msg) {
-  var app = msg.path
-
-  // disable then enable 1 extension or app
-  function reload (ext) {
-    // false for disable, true for enable
-    chrome.management.setEnabled(ext.id, false, function() {
-      chrome.management.setEnabled(ext.id, true, function() {
-        // func to tell extension it was auto-reloaded
-        function forwardMsg(){
-          chrome.runtime.sendMessage(ext.id, msg)
-        }
-        // apps still need launch
-        if (ext.type.match(/app/))
-          chrome.management.launchApp(ext.id, forwardMsg)
-        else forwardMsg()
-        console.log(ext.name + ' reloaded')
-        // show an 'OK' badge
-        badge.tempSet('ok')
-      })
-    })
-  }
-
-  chrome.management.getAll(function(extensions) {
-    extensions = extensions.filter(function (ext){
-      return (
-        // installed as unpacked folder
-        (ext.installType==='development')
-        // if app not specified, only reload enabled
-        && (app || ext.enabled===true)
-        // restrict to app name or id if given
-        // && ( (!app) || app===ext.name
-        //   || app===ext.id)
-        // Clerc shouldn't reload itself
-        && (ext.id !==chrome.runtime.id)
-      )
-    })
-    extensions.forEach(reload)
-  })
-}
+const badge = makeBadge({chrome})
+const reload = makeReload({chrome})
 
 //////////////////////////////////////////////
 
+const reloadExtensions = async msg => {
+  console.log('msg',msg)
+  const reloaded = await reload(msg)
+  if (reloaded.length)
+    badge.tempSet('ok')
+  reloaded.forEach(async ext=>
+    console.log(`reloaded ${ext.name}`))
+}
+
 const messenger = {
   message (msg){
-    console.log('messenger', msg)
     if (msg && msg.error)
       return onError(msg.error)
-    reloadExtensions(msg)
+
+    if (msg && msg.command === 'reload')
+      reloadExtensions(msg)
   },
 }
 
 const onOpen = ()=> {
   console.log('connected to livereload')
-  badge.set('ok')
-  badge.delay('connected', 1200)
+  badge.set('connected')
+  badge.tempSet('ok', 1200)
   chrome.browserAction.onClicked.removeListener(connect)
   chrome.browserAction.onClicked.addListener(disconnect)
 }
 
 const onClose = ()=> {
   console.log('disconnected from livereload')
-  badge.set('hide')
+  if (badge.activeBadge() !== 'no') {
+    badge.set('hide')
+    badge.tempSet('disconnected', 1200)
+  }
   chrome.browserAction.onClicked.removeListener(disconnect)
   chrome.browserAction.onClicked.addListener(connect)
 }
 
 const onError = err=> {
-  badge.set('no')
-  badge.delay('hide', 1200)
+  badge.set('hide')
+  badge.tempSet('no', 1900)
   console.error(err)
 }
 
@@ -123,10 +61,11 @@ const lrSocket = liveReloadSocket({
 
 const connect = ()=> lrSocket.connect()
 const disconnect = ()=> lrSocket.disconnect()
-chrome.browserAction.onClicked.addListener(()=> lrSocket.connect())
+
+chrome.browserAction.onClicked.addListener(()=> {
+  badge.set('disconnected')
+  lrSocket.connect()
+})
 
 
-
-
-
-////
+//
